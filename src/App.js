5a22628eac891dc84edf62de932d24e9f2b81c53
Duc Trung <sductrung@gmail.com>
2021-09-14 22:53:26 +0700

    complete counter state using redux basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useSelector, useDispatch} from 'react-redux';
+import {increase, decrease} from './actions/counter.js'
 function App() {
+  const count = useSelector(state => state.counter)
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Current count: {count}</h1>
+      <button onClick={() => dispatch(increase(5))}>Add more</button>
+      <button onClick={() => dispatch(decrease(5))}>Decrease more</button>
     </div>
   );
 }
